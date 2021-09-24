93e5444e4c0816b9287abc6eaaeef393fddade55
Bernard Opiyo <brobernard.254@gmail.com>
2021-09-24 13:43:34 +0300

    add main folders

--- app/__init__.py
+++ app/__init__.py
@@ -1,16 +1,58 @@
-from flask import Flask
+from flask import Flask, app
+from flask_bootstrap import Bootstrap
+from config import config_options
 from flask_sqlalchemy import SQLAlchemy
-from os import path
+from flask_uploads import UploadSet,configure_uploads,IMAGES
+from flask_mail import Mail
 from flask_login import LoginManager
+from flask_simplemde import SimpleMDE
 
-def create_app():
+
+
+bootstrap = Bootstrap()
+db = SQLAlchemy()
+mail = Mail()
+photos = UploadSet('photos',IMAGES)
+simple = SimpleMDE()
+
+
+
+login_manager = LoginManager()
+login_manager.session_protection = 'strong' 
+#provides different security levels.
+login_manager.login_view = 'auth.login'
+
+
+def create_app(config_name):
+
+    # inititalizing application
     app = Flask(__name__)
-    app.config['SECRETE_KEY'] = "hellow world"
+
+ 
+    # creating the app configurations
+    app.config.from_object(config_options[config_name])
+
+    # Initializing flask extensions
+    bootstrap.init_app(app)
+    db.init_app(app)
+    login_manager.init_app(app)
+    mail.init_app(app)
+    simple.init_app(app)
 
 
-    @app.route("/")
-    def home():
-         
+    #registering the blueprint
+    from .main import main as main_blueprint
+    app.register_blueprint(main_blueprint)
+
+    from .auth import auth as auth_blueprint
+    app.register_blueprint(auth_blueprint,url_prefix = '/auth')
+
+    #setting config
+   
+    # configure UploadSet
+    configure_uploads(app,photos)
+    
+    return app
 
 
-     return app
+
--- manage.py
+++ manage.py
@@ -1,6 +1,42 @@
 from app import create_app
-
+from app import create_app,db
+from  flask_migrate import Migrate, MigrateCommand
+from flask_script import Manager,Server
+from app.models import User,Role
 
 if __name__ == "__main__":
     app = create_app()
     app.run(debug=True)
+
+
+# creating app instance
+app = create_app('development')
+# app = create_app('production')
+# app = create_app('test')
+
+
+migrate = Migrate(app,db)
+manager = Manager(app)
+
+manager.add_command('server',Server)
+manager.add_command('db', MigrateCommand)
+
+
+@manager.command
+
+def test():
+
+    """
+    Run the unit tests.
+
+    """
+    import unittest
+    tests = unittest.TestLoader().discover('test')
+    unittest.TextTestRunner(verbosity=2).run(tests)
+
+@manager.shell
+def make_shell_context():
+    return dict(app = app,db = db,User = User,Role = Role ) 
+
+if __name__ == '__main__':
+    manager.run()
